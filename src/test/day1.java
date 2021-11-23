edb7bb0fdc89e977441643fb739a05cc5363481a
hari <hari081189@gmail.com>
2021-11-23 18:14:53 +0530

    initial load

---
+++ src/test/day1.java
@@ -0,0 +1,19 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class day1 {
+	
+	@Test
+	public void demo1()
+	{
+		System.out.println("hello!!!");
+	}
+	
+	@Test
+	public void demo2()
+	{
+		System.out.println("world!!!");
+	}
+
+}
